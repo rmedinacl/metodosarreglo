853f3632295c88fbc51f98e2810790721e186391
Rodrigo Medina P <rmedina@mountain-creative.com>
2024-03-31 10:20:55 -0300

    desarrollo desafio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,73 @@
-const listaDeInvitados = document.getElementById("nuevoInvitado");
-const invitadoInput = document.getElementById("nuevoInvitado");
-const botonAgregar = document.getElementById("agregarInvitado");
+let tasks = [
+  { id: "5", description: "Levantarse 04:30 AM", completed: true },
+  { id: "7", description: "Llenar Calendario Matutino", completed: true },
+  { id: "9", description: "Hacer Tarea Más Rapida", completed: false },
+];
 
-const invitados = [];
-botonAgregar.addEventListener("click", () => {
-  const nuevoInvitado = invitadoInput.value;
-  invitados.push(nuevoInvitado);
-  invitadoInput.value = "";
-  renderizarLista();
-});
+function renderTasks() {
+  const taskList = document.getElementById("taskList");
+  taskList.innerHTML = "";
+
+  tasks.forEach((task) => {
+    const li = document.createElement("li");
+    li.innerHTML = `
+      <p>id: ${task.id}</p>
+      <h4>Tarea:${task.description}</h4>
+      <input type="checkbox" ${
+        task.completed ? "checked" : ""
+      } onchange="toggleTaskStatus(${task.id})">
+      <span class="${task.completed ? "completed" : ""}">${
+      task.completed ? "Realizado" : "Pendiente"
+    }</span>
+      <button onclick="deleteTask(${task.id})">Eliminar</button>
+    `;
+    taskList.appendChild(li);
+  });
+
+  updateCounters();
+}
+
+function addTask() {
+  const taskInput = document.getElementById("taskInput");
+  const description = taskInput.value.trim();
+
+  if (description !== "") {
+    const newTask = {
+      id: tasks.length + 1,
+      description: description,
+      completed: false,
+    };
+    tasks.push(newTask);
+    taskInput.value = "";
+    renderTasks();
+  } else {
+    alert(
+      "No se permiten tareas en blanco. Por favor, ingrese una tarea valida"
+    );
+  }
+}
+
+function deleteTask(taskId) {
+  tasks = tasks.filter((task) => task.id !== taskId);
+  renderTasks();
+}
+
+function toggleTaskStatus(taskId) {
+  tasks = tasks.map((task) => {
+    if (task.id === taskId) {
+      return { ...task, completed: !task.completed };
+    }
+    return task;
+  });
+  renderTasks();
+}
+
+function updateCounters() {
+  const totalTasks = tasks.length;
+  const completedTasks = tasks.filter((task) => task.completed).length;
+
+  document.getElementById("totalTasks").textContent = totalTasks;
+  document.getElementById("completedTasks").textContent = completedTasks;
+}
+
+renderTasks();
